6d120ff4c52028639ddd49e43bed399f8c845ec3
Rafael <rafaelnewcode@gmail.com>
2022-03-01 21:13:54 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fragrant",
+    version="0.0.1",
+    packages=find_packages(where='.'),
+    package_dir={'':'.'},
+    description="web python framework",
+    url="https://github.com/rafaelcode0337/fragrant"
+)
