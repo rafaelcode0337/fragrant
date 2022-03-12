1a5c53007c3aaed5ff783a7e0ad1959c8a7dd076
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2022-03-12 08:40:59 -0300

    Criação de protótipos de eventos

--- core/models/fragrant_teste_model.py
+++ core/models/fragrant_teste_model.py
@@ -1,9 +1,10 @@
 
-from fragrant_model_abstract import FragrantModelAbstract as modelabstract, fieldalias, fieldmask, fillable, rules
+from fragrant_model_abstract import Afterpost, BeforeEdit, Beforepost, FragrantModelAbstract as modelabstract, fieldalias, fieldmask, fillable, rules
 from fragrant_model_rules import RulesEnum as Rules
 
 class Cliente(modelabstract):
   def __init__(self): 
+      
      fillable([                        
             'nome',
             'razao',
@@ -24,7 +25,23 @@ class Cliente(modelabstract):
      rules({
          'cnpj',Rules.notnull,
          'nome',Rules.uniquekey
-     })  
+     }) 
+     
+     # Eventos - Programação Orientada a Eventos 
+     # Programação orientada a eventos é um paradigma de programação. Diferente de programas tradicionais 
+     # que seguem um fluxo de controle padronizado, o controle de fluxo de programas orientados a evento 
+     # são guiados por indicações externas, chamadas eventos
+     
+     # Eventos que serão Assinados por classes como Controllers ou Provedores de serviços para 
+     # Manipulação dos eventos Single-Thread do Modelo , nao é um Padrão Observable, são eventos de 
+     # Insântia única que respondem a um comportamento específico 
+     # After Post é executado antes de acionar a Transação Post do DAO com o Banco de dados , 
+     # Você pode definir ações para sua aplicação.
+     Afterpost()
+     
+     Beforepost()
+     
+     BeforeEdit()
         
    
           
